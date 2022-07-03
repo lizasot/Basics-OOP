5b13e7280581eb7395adf162bde64434f7284d29
Krist <Kri2t@yandex.ru>
2022-07-03 15:22:27 +0300

    Выполнена задача 2.1

---
+++ BasicsOOP/BankAccount.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicsOOP;
+
+public enum BankAccountType
+{
+    Payment,
+    Current,
+    Deposit,
+    Currency,
+    Special,
+    Budget,
+}
+
+public class BankAccount
+{
+    private int BankNumber;
+    private decimal Balance;
+    private BankAccountType TypeAccount;
+
+
+    public void SetBankNumber(int bankNumber)
+    {
+        BankNumber = bankNumber;
+    }
+    public int GetBankNumber()
+    {
+        return BankNumber;
+    }
+    public void SetBalance(decimal balance)
+    {
+        Balance = balance;
+    }
+    public decimal GetBalance()
+    {
+        return Balance;
+    }
+    public void SetTypeAccount(BankAccountType typeAccount)
+    {
+        TypeAccount = typeAccount;
+    }
+    public BankAccountType GetTypeAccount()
+    {
+        return TypeAccount;
+    }
+}
--- BasicsOOP/Program.cs
+++ BasicsOOP/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BasicsOOP;
+
+BankAccount test = new BankAccount();
+
+test.SetBankNumber(1);
+test.SetBalance(100);
+test.SetTypeAccount(BankAccountType.Payment);
+
+Console.WriteLine($"Баланс банковского счёта #{test.GetBankNumber()} типа {test.GetTypeAccount()}: {test.GetBalance()};");
