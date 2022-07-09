4d556563cb6a41e4f510f24bdbf48e377491ed8b
Krist <Kri2t@yandex.ru>
2022-07-09 15:18:42 +0300

    Реализован метод поиска Email в строке

--- BasicsOOP/ProcessingString.cs
+++ BasicsOOP/ProcessingString.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Дан текстовый файл, содержащий ФИО и e-mail адрес. 
+//Разделителем между ФИО и адресом электронной почты является символ &: 
+//Кучма Андрей Витальевич & Kuchma@mail.ru Мизинцев Павел Николаевич & Pasha@mail.ru
+//Сформировать новый файл, содержащий список адресов электронной почты
+
 namespace BasicsOOP
 {
     /// <summary>
@@ -24,6 +29,57 @@ namespace BasicsOOP
                 result.Append(str[i]);
             }
             return result.ToString();
+        }
+        /// <summary>
+        /// Возвращает первый Email в строке 
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string SearchMail(ref string str)
+        {
+            //[ ][n символов]@[n символов].[n символов][ ]
+            int index = str.IndexOf('@');
+            var indexStart = index;
+            try
+            {
+                if (index > 0)
+                {
+                    //находит первый символ, с которого начинается адрес почты
+                    do
+                    {
+                        indexStart--;
+                    } while (str[indexStart] != ' ' && indexStart != 0);
+                    if (str[indexStart] == ' ')
+                    {
+                        indexStart++;
+                    }
+
+                    //проверяет наличие точки в адресе почты
+                    do
+                    {
+                        index++;
+                    } while (str[index] != '.' && str[index] != ' ' && index != str.Length - 1);
+                    if (str[index] == ' ' || index == str.Length - 1)
+                    {
+                        return ""; //возвращает пустую строку, если точка не найдена
+                    }
+                    //находит последний символ адреса почты
+                    do
+                    {
+                        index++;
+                    } while (str[index] != ' ' && index != str.Length - 1);
+                    if (str[index] == ' ')
+                    {
+                        index--;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return "";
+            }
+            return str.Substring(indexStart, index);
         }
     }
 }
--- BasicsOOP/Program.cs
+++ BasicsOOP/Program.cs
@@ -1,34 +1,8 @@
 ﻿using BasicsOOP;
 
-Console.WriteLine("Введите строку, которую необходимо перевернуть");
-Console.WriteLine(ProcessingString.Reverse(Console.ReadLine()));
-Console.Write("пёс: ");
-Console.WriteLine(ProcessingString.Reverse("пёс"));
-Console.Write("tacocat: ");
-Console.WriteLine(ProcessingString.Reverse("tacocat"));
-Console.Write("1234321: ");
-Console.WriteLine(ProcessingString.Reverse("1234321"));
-Console.Write("3432112: ");
-Console.WriteLine(ProcessingString.Reverse("3432112"));
-
-/*
-BankAccount test1 = new BankAccount();
-BankAccount test2 = new BankAccount(413);
-
-test1.PrintInfo();
-Console.WriteLine("Введите сумму, которую хотите положить на счёт #1");
-test1.BalancePut(Convert.ToDecimal(Console.ReadLine()));
-test1.PrintInfo();
-test2.PrintInfo();
-Console.WriteLine("Введите сумму, которую хотите перевести со счёта #1 на счёт #2");
-if (test2.TransferMoney(test1, Convert.ToDecimal(Console.ReadLine())))
+Console.WriteLine("Введите строку, в которой необходимо найти Email");
+string str = Console.ReadLine();
+if (str != null)
 {
-    Console.WriteLine("Перевод прошёл успешло.");
+    Console.WriteLine(ProcessingString.SearchMail(ref str));
 }
-else
-{
-    Console.WriteLine("Не хватает средств для перевода.");
-}
-test1.PrintInfo();
-test2.PrintInfo();
-*/
