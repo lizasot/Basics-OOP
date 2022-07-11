0ddd8d7dd332219b8d35f94320c0849316c9b21b
Krist <Kri2t@yandex.ru>
2022-07-11 23:34:50 +0300

    StringBuilder заменён на более подходящий здесь массив символов; Убраны сообщения в консоль там, где это не нужно.

--- BasicsOOP/Program.cs
+++ BasicsOOP/Program.cs
@@ -1,5 +1,12 @@
 ﻿using BasicsOOP;
 
-Console.WriteLine("Введите путь файла:");
-string path = Console.ReadLine();
-ProcessingString.ReadFile(path);
+Console.WriteLine("Введите строку, которую необходимо перевернуть");
+Console.WriteLine(ProcessingString.Reverse(Console.ReadLine()));
+Console.Write("пёс: ");
+Console.WriteLine(ProcessingString.Reverse("пёс"));
+Console.Write("tacocat: ");
+Console.WriteLine(ProcessingString.Reverse("tacocat"));
+Console.Write("1234321: ");
+Console.WriteLine(ProcessingString.Reverse("1234321"));
+Console.Write("3432112: ");
+Console.WriteLine(ProcessingString.Reverse("3432112"));
