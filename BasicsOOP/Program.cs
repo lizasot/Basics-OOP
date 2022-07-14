d4efab5f5fee2665b161fc0de15e0be43af52997
Krist <Kri2t@yandex.ru>
2022-07-14 18:03:10 +0300

    Выполнена задача 5.1 и протестирована

--- BasicsOOP/Program.cs
+++ BasicsOOP/Program.cs
@@ -1,36 +1,179 @@
 ﻿using BasicsOOP;
-using Builds;
 
-var b = Creator.CreateBuild(188, 5, 160, 4);
-var b2 = Creator.CreateBuild(188, 5, 160, 4);
-Creator.CreateBuild(150);
-Creator.CreateBuild(10);
-Creator.CreateBuild(80);
+var numb23 = new RationalNumber(2, 3); //0,66(..67)
+var numb58_1 = new RationalNumber(5, 8); //0,625
+var numb58_2 = new RationalNumber(5, 8); //0,625
+var numb21_18 = new RationalNumber(21, 18); //1,166(..67)
+// 21/18 > 2/3 > 5/8
 
-Console.WriteLine(b.ID);
-Console.WriteLine(b2.ID);
-Console.WriteLine(b.ApartmentsPerFloor());
-Console.WriteLine(b.FloorHeight());
-
-Console.WriteLine();
-Console.WriteLine("Total builds: " + Creator.TotalBuild);
-Console.WriteLine("ID and Height of each building:");
-foreach (var build in Creator.BuildList)
+static void SetDefault(ref RationalNumber numb23, ref RationalNumber numb58_1, ref RationalNumber numb58_2, ref RationalNumber numb21_18)
 {
-    Console.WriteLine(build.Key + " " + build.Value.Height);
+    numb23.Numerator = 2;
+    numb23.Denominator = 3;
+
+    numb58_1.Numerator = 5;
+    numb58_1.Denominator = 8;
+
+    numb58_2.Numerator = 5;
+    numb58_2.Denominator = 8;
+
+    numb21_18.Numerator = 21;
+    numb21_18.Denominator = 18;
 }
+//SetDefault(ref numb23, ref numb58_1, ref numb58_2, ref numb21_18);
 
-Console.WriteLine();
-Console.Write("Введите айди здания, которое хотите удалить: ");
-if (!Creator.RemoveBuild(int.Parse(Console.ReadLine())))
+var tmp = new RationalNumber();
+
+//Тест в виде: Выводит на экран всегда true, если результат совпадает с ожидаемым
+//Команда в формате: ВыводНаЭкран(func() == ожидаемый_результат);
+
+try
 {
-    Console.WriteLine("Нет такого айди.");
+
+    //==
+    Console.WriteLine("\nТест команды ==");
+    Console.WriteLine((numb58_1 == numb23) == false);
+    Console.WriteLine((numb58_1 == numb58_2) == true);
+    Console.WriteLine((numb23 == numb21_18) == false);
+
+    //!=
+    Console.WriteLine("\nТест команды !=");
+    Console.WriteLine((numb58_1 != numb23) == true);
+    Console.WriteLine((numb58_1 != numb58_2) == false);
+    Console.WriteLine((numb23 != numb21_18) == true);
+
+    //<
+    Console.WriteLine("\nТест команды <");
+    Console.WriteLine((numb58_1 < numb23) == true);
+    Console.WriteLine("======");
+    Console.WriteLine((numb58_1 == numb58_2) == true);
+    Console.WriteLine(numb58_1 == numb58_2);
+    Console.WriteLine((numb58_1 < numb58_2) == false);
+    Console.WriteLine("======");
+    Console.WriteLine((numb23 < numb21_18) == true);
+
+    //>
+    Console.WriteLine("\nТест команды >");
+    Console.WriteLine((numb58_1 > numb23) == false);
+    Console.WriteLine((numb58_1 > numb58_2) == false);
+    Console.WriteLine((numb23 > numb21_18) == false);
+
+    //<=
+    Console.WriteLine("\nТест команды <=");
+    Console.WriteLine((numb58_1 <= numb23) == true);
+    Console.WriteLine((numb58_1 <= numb58_2) == true);
+    Console.WriteLine((numb23 <= numb21_18) == true);
+
+    //>=
+    Console.WriteLine("\nТест команды >=");
+    Console.WriteLine((numb58_1 >= numb23) == false);
+    Console.WriteLine((numb58_1 >= numb58_2) == true);
+    Console.WriteLine((numb23 >= numb21_18) == false);
+
+    //+
+    Console.WriteLine("\nТест команды +");
+    tmp.Numerator = 31;
+    tmp.Denominator = 24;
+    Console.WriteLine((numb58_1 + numb23) == tmp); // 31/24
+
+    tmp.Numerator = 10;
+    tmp.Denominator = 8;
+    Console.WriteLine((numb58_1 + numb58_2) == tmp); // 10/8
+
+    tmp.Numerator = 33;
+    tmp.Denominator = 18;
+    Console.WriteLine((numb23 + numb21_18) == tmp); // 33/18
+
+    //-
+    Console.WriteLine("\nТест команды -");
+    tmp.Numerator = -1;
+    tmp.Denominator = 24;
+    Console.WriteLine((numb58_1 - numb23) == tmp); // -1/24
+
+    tmp.Numerator = 0;
+    tmp.Denominator = 8;
+    Console.WriteLine((numb58_1 - numb58_2) == tmp); // 0/8
+
+    tmp.Numerator = -9;
+    tmp.Denominator = 18;
+    Console.WriteLine((numb23 - numb21_18) == tmp); // -9/18
+
+    //++
+    Console.WriteLine("\nТест команды ++");
+    tmp.Numerator = numb23.Numerator + numb23.Denominator;
+    tmp.Denominator = numb23.Denominator;
+    numb23++;
+    Console.WriteLine(numb23 == tmp);
+
+    tmp.Numerator = numb58_1.Numerator + numb58_1.Denominator;
+    tmp.Denominator = numb58_1.Denominator;
+    numb58_1++;
+    Console.WriteLine(numb58_1 == tmp);
+
+    tmp.Numerator = numb21_18.Numerator + numb21_18.Denominator;
+    tmp.Denominator = numb21_18.Denominator;
+    numb21_18++;
+    Console.WriteLine(numb21_18 == tmp);
+
+    //--
+    Console.WriteLine("\nТест команды --");
+    tmp.Numerator = numb23.Numerator - numb23.Denominator;
+    tmp.Denominator = numb23.Denominator;
+    numb23--;
+    Console.WriteLine(numb23 == tmp);
+
+    tmp.Numerator = numb58_1.Numerator - numb58_1.Denominator;
+    tmp.Denominator = numb58_1.Denominator;
+    numb58_1--;
+    Console.WriteLine(numb58_1 == tmp);
+
+    tmp.Numerator = numb21_18.Numerator - numb21_18.Denominator;
+    tmp.Denominator = numb21_18.Denominator;
+    numb21_18--;
+    Console.WriteLine(numb21_18 == tmp);
+
+    //SetDefault(ref numb23, ref numb58_1, ref numb58_2, ref numb21_18);
+
+    //ToString()
+    Console.WriteLine("\nТест команды ToString()");
+    Console.WriteLine("numb23 = " + numb23);
+    Console.WriteLine("numb58_1 = " + numb58_1);
+    Console.WriteLine("numb21_18 = " + numb21_18);
+
+    //float
+    Console.WriteLine("\nТест команды (float)");
+    Console.WriteLine("(float)numb23 = " + (float)numb23);
+    Console.WriteLine("(float)numb58_1 = " + (float)numb58_1);
+    Console.WriteLine("(float)numb21_18 = " + (float)numb21_18);
+
+    //int
+    Console.WriteLine("\nТест команды (int)");
+    Console.WriteLine("(int)numb23 = " + (int)numb23);
+    Console.WriteLine("(int)numb58_1 = " + (int)numb58_1);
+    Console.WriteLine("(int)numb21_18 = " + (int)numb21_18);
+
+    //*
+    Console.WriteLine("\nТест команды *");
+    Console.WriteLine((numb58_1 * numb23) == new RationalNumber(10, 24));
+    Console.WriteLine("======");
+    Console.WriteLine(numb58_1 * numb58_2);
+    Console.WriteLine((numb58_1 * numb58_2) == new RationalNumber(25, 64));
+    Console.WriteLine("======");
+    Console.WriteLine((numb23 * numb21_18) == new RationalNumber(42, 54));
+
+    //"/"
+    Console.WriteLine("\nТест команды /");
+    Console.WriteLine((numb58_1 / numb23) == new RationalNumber(15, 16));
+    Console.WriteLine((numb58_1 / numb58_2) == new RationalNumber(40, 40));
+    Console.WriteLine((numb23 / numb21_18) == new RationalNumber(36, 63));
+
+    //%
+    Console.WriteLine("\nТест команды %");
+    Console.WriteLine((numb58_1 % numb23) == new RationalNumber(15, 16));
+    Console.WriteLine((numb58_1 % numb58_2) == new RationalNumber(0, 40));
+    Console.WriteLine((numb23 % numb21_18) == new RationalNumber(36, 63));
 }
-
-
-Console.WriteLine();
-Console.WriteLine("ID and Height of each building:");
-foreach (var build in Creator.BuildList)
+catch (Exception e)
 {
-    Console.WriteLine(build.Key + " " + build.Value.Height);
+    Console.WriteLine(e.Message);
 }
---
+++ BasicsOOP/RationalNumber.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicsOOP;
+public class RationalNumber
+{
+    private int _Numerator;
+    private int _Denominator;
+
+    public int Numerator
+    {
+        get { return _Numerator; }
+        set { _Numerator = value; }
+    }
+    public int Denominator
+    {
+        get { return _Denominator; }
+        set
+        {
+            if (value == 0)
+            {
+                throw new ArgumentOutOfRangeException("value");
+            }
+            if (value < 0)
+            {
+                _Numerator *= -1;
+                _Denominator = value * (-1);
+            }
+            _Denominator = value;
+        }
+    }
+
+    public static int NOD(int numb1, int numb2)
+    {
+        int tmp = 0;
+        if (numb1 == 0)
+        {
+            throw new ArgumentOutOfRangeException("numb1");
+        }
+        if (numb2 == 0)
+        {
+            throw new ArgumentOutOfRangeException("numb2");
+        }
+        if (numb1 < 0)
+        {
+            numb1 *= -1;
+        }
+        if (numb2 < 0)
+        {
+            numb1 *= -1;
+        }
+        if (numb2 > numb1)
+        {
+            tmp = numb1;
+            numb1 = numb2;
+            numb2 = tmp;
+        }
+        tmp = 0;
+        int result = numb1;
+        int remainder = numb2;
+        do
+        {
+            tmp = result;
+            result = remainder;
+            remainder = tmp - (result * (tmp / result));
+        } while (remainder != 0);
+        return result;
+    }
+    public static int NOK(int numb1, int numb2)
+    {
+        return (numb1 * numb2) / NOD(numb1, numb2);
+    }
+
+    public static bool operator ==(RationalNumber numb1, RationalNumber numb2)
+    {
+        return ((numb1._Numerator == numb2._Numerator) && (numb1._Denominator == numb2._Denominator));
+    }
+    public static bool operator !=(RationalNumber numb1, RationalNumber numb2) => !(numb1 == numb2);
+    public static bool operator >(RationalNumber numb1, RationalNumber numb2)
+    {
+        if (numb1._Denominator == numb2._Denominator)
+        {
+            return numb1._Numerator > numb2._Numerator;
+        }
+        if (numb1._Numerator == numb2._Numerator)
+        {
+            return !(numb1._Denominator > numb2._Denominator);
+        }
+
+        int nok = NOK(numb1._Denominator, numb2._Denominator);
+        if (numb1._Denominator * (nok / numb1._Denominator) == numb2._Denominator * (nok / numb2._Denominator)) //знаменатели приведённых дробей должны быть равны
+        {
+            return (numb1._Numerator * (nok / numb1._Denominator)) > (numb2._Numerator * (nok / numb2._Denominator));
+        }
+        else
+        {
+            throw new Exception("Возник непредвиденный случай.");
+        }
+    }
+    public static bool operator <(RationalNumber numb1, RationalNumber numb2)
+    {
+        if (numb1 == numb2)
+        {
+            return false;
+        }
+        else
+        {
+            return !(numb1 > numb2);
+        }
+    }
+    public static bool operator >=(RationalNumber numb1, RationalNumber numb2)
+    {
+        if (numb1 == numb2)
+        {
+            return true;
+        }
+        else
+        {
+            return numb1 > numb2;
+        }
+    }
+    public static bool operator <=(RationalNumber numb1, RationalNumber numb2)
+    {
+        if (numb1 == numb2)
+        {
+            return true;
+        }
+        else
+        {
+            return numb1 < numb2;
+        }
+    }
+    public static RationalNumber operator +(RationalNumber numb1, RationalNumber numb2)
+    {
+        int nok = NOK(numb1._Denominator, numb2._Denominator);
+        return new RationalNumber((numb1._Numerator * (nok / numb1._Denominator)) + (numb2._Numerator * (nok / numb2._Denominator)), nok);
+    }
+    public static RationalNumber operator -(RationalNumber numb1, RationalNumber numb2)
+    {
+        int nok = NOK(numb1._Denominator, numb2._Denominator);
+        return new RationalNumber((numb1._Numerator * (nok / numb1._Denominator)) - (numb2._Numerator * (nok / numb2._Denominator)), nok);
+    }
+    public static RationalNumber operator ++(RationalNumber numb)
+    {
+        numb._Numerator += numb._Denominator;
+        return numb;
+    }
+    public static RationalNumber operator --(RationalNumber numb)
+    {
+        numb._Numerator -= numb._Denominator;
+        return numb;
+    }
+    public static RationalNumber operator *(RationalNumber numb1, RationalNumber numb2)
+    {
+        return new RationalNumber(numb1._Numerator * numb2._Numerator, numb1._Denominator * numb2._Denominator);
+    }
+    public static RationalNumber operator /(RationalNumber numb1, RationalNumber numb2)
+    {
+        return new RationalNumber(numb1._Numerator * numb2._Denominator, numb1._Denominator * numb2._Numerator);
+    }
+    public static RationalNumber operator %(RationalNumber numb1, RationalNumber numb2)
+    {
+        var numb = (numb1 / numb2);
+        numb.Numerator -= (numb.Numerator / numb.Denominator)* numb.Denominator;
+        return numb;
+    }
+
+    public override string ToString()
+    {
+        return _Numerator.ToString() + "/" + _Denominator.ToString();
+    }
+    public static explicit operator float(RationalNumber numb)
+    {
+        return (float)numb._Numerator / (float)numb._Denominator;
+    }
+    public static explicit operator int(RationalNumber numb)
+    {
+        return numb._Numerator / numb._Denominator;
+    }
+
+    public RationalNumber()
+    {
+        Numerator = 1;
+        Denominator = 1;
+    }
+    public RationalNumber(int numerator, int denominator)
+    {
+        Numerator = numerator;
+        Denominator = denominator;
+    }
+}
