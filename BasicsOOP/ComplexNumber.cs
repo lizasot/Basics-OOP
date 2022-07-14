ab60a72827338bad4c0f180608c99418756afa22
Krist <Kri2t@yandex.ru>
2022-07-14 18:40:15 +0300

    Выполнена  задача 5.2 и протестирована

---
+++ BasicsOOP/ComplexNumber.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicsOOP;
+public class ComplexNumber
+{
+    private float _Real;
+    private float _Imaginary;
+
+    public float Real
+    {
+        get { return _Real; }
+        set { _Real = value; }
+    }
+    public float Imaginary
+    {
+        get { return _Imaginary; }
+        set { _Imaginary = value; }
+    }
+
+    public static bool operator ==(ComplexNumber numb1, ComplexNumber numb2)
+    {
+        return numb1._Real == numb2._Real && numb1._Imaginary == numb2._Imaginary;
+    }
+    public static bool operator !=(ComplexNumber numb1, ComplexNumber numb2) => !(numb1 == numb2);
+    public static ComplexNumber operator +(ComplexNumber numb1, ComplexNumber numb2)
+    {
+        return new ComplexNumber(numb1._Real + numb2._Real, numb1._Imaginary + numb2._Imaginary);
+    }
+    public static ComplexNumber operator *(ComplexNumber numb1, ComplexNumber numb2)
+    {
+        return new ComplexNumber((numb1._Real * numb2._Real) - (numb1._Imaginary * numb2._Imaginary), (numb1._Real * numb2._Imaginary) + (numb1._Imaginary * numb2._Real));
+    }
+    public static ComplexNumber operator -(ComplexNumber numb1, ComplexNumber numb2)
+    {
+        return new ComplexNumber(numb1._Real - numb2._Real, numb1._Imaginary - numb2._Imaginary);
+    }
+    public override string ToString()
+    {
+        string str = "";
+        if (_Real != 0)
+        {
+            str = _Real.ToString();
+        }
+        if (_Imaginary != 0)
+        {
+            if (_Imaginary > 0)
+            {
+                str += " + ";
+            }
+            else
+            {
+                str += " - ";
+                _Imaginary *= -1;
+            }
+            str += _Imaginary != 1 ? _Imaginary.ToString() : "" + "i";
+        }
+        return str;
+    }
+
+    public ComplexNumber()
+    {
+        Real = 0;
+        Imaginary = 0;
+    }
+    public ComplexNumber(float real, float imaginary)
+    {
+        Real = real;
+        Imaginary = imaginary;
+    }
+}
--- BasicsOOP/Program.cs
+++ BasicsOOP/Program.cs
@@ -1,10 +1,44 @@
 ﻿using BasicsOOP;
 
-var numb23 = new RationalNumber(2, 3); //0,66(..67)
-var numb58_1 = new RationalNumber(5, 8); //0,625
-var numb58_2 = new RationalNumber(5, 8); //0,625
-var numb21_18 = new RationalNumber(21, 18); //1,166(..67)
-// 21/18 > 2/3 > 5/8
+static void TestComplexNumber()
+{
+    var numb1 = new ComplexNumber(4, 2);
+    var numb2 = new ComplexNumber(-5, 3);
+
+    Console.WriteLine("\nТест команды +");
+    numb1.Real = 4; numb1.Imaginary = 2;
+    numb2.Real = -5; numb2.Imaginary = 3;
+    Console.WriteLine(numb1 + " + " + numb2 + " = " + (numb1+ numb2));
+    Console.Write("Совпадает ли с ожидаемым результатом: ");
+    Console.WriteLine(numb1 + numb2 == new ComplexNumber(-1, 5));
+
+    Console.WriteLine("\nТест команды *");
+    numb1.Real = 2; numb1.Imaginary = 3;
+    numb2.Real = 5; numb2.Imaginary = 4;
+    Console.WriteLine(numb1 + " * " + numb2 + " = " + (numb1 * numb2));
+    Console.Write("Совпадает ли с ожидаемым результатом: ");
+    Console.WriteLine(numb1 * numb2 == new ComplexNumber(-2, 23));
+
+    Console.WriteLine("\nТест команды -");
+    numb1.Real = 5; numb1.Imaginary = 4;
+    numb2.Real = -2; numb2.Imaginary = 3;
+    Console.WriteLine(numb1 + " - " + numb2 + " = " + (numb1 - numb2));
+    Console.Write("Совпадает ли с ожидаемым результатом: ");
+    Console.WriteLine(numb1 - numb2 == new ComplexNumber(7, 1));
+
+    Console.WriteLine("\nТест команды ==");
+    numb1.Real = 5; numb1.Imaginary = 4;
+    numb2.Real = -2; numb2.Imaginary = 3;
+    Console.WriteLine(numb1 + " == " + numb2 + " = " + (numb1 == numb2));
+    Console.Write("Совпадает ли с ожидаемым результатом: ");
+    Console.WriteLine((numb1 == numb2) == false);
+
+    numb1.Real = -2; numb1.Imaginary = 3;
+    numb2.Real = -2; numb2.Imaginary = 3;
+    Console.WriteLine(numb1 + " == " + numb2 + " = " + (numb1 == numb2));
+    Console.Write("Совпадает ли с ожидаемым результатом: ");
+    Console.WriteLine((numb1 == numb2) == true);
+}
 
 static void SetDefault(ref RationalNumber numb23, ref RationalNumber numb58_1, ref RationalNumber numb58_2, ref RationalNumber numb21_18)
 {
@@ -21,159 +55,170 @@ static void SetDefault(ref RationalNumber numb23, ref RationalNumber numb58_1, r
     numb21_18.Denominator = 18;
 }
 //SetDefault(ref numb23, ref numb58_1, ref numb58_2, ref numb21_18);
-
-var tmp = new RationalNumber();
-
-//Тест в виде: Выводит на экран всегда true, если результат совпадает с ожидаемым
-//Команда в формате: ВыводНаЭкран(func() == ожидаемый_результат);
-
-try
+static void TestRationalNumber()
 {
+    var numb23 = new RationalNumber(2, 3); //0,66(..67)
+    var numb58_1 = new RationalNumber(5, 8); //0,625
+    var numb58_2 = new RationalNumber(5, 8); //0,625
+    var numb21_18 = new RationalNumber(21, 18); //1,166(..67)
+    // 21/18 > 2/3 > 5/8
+    var tmp = new RationalNumber();
 
-    //==
-    Console.WriteLine("\nТест команды ==");
-    Console.WriteLine((numb58_1 == numb23) == false);
-    Console.WriteLine((numb58_1 == numb58_2) == true);
-    Console.WriteLine((numb23 == numb21_18) == false);
 
-    //!=
-    Console.WriteLine("\nТест команды !=");
-    Console.WriteLine((numb58_1 != numb23) == true);
-    Console.WriteLine((numb58_1 != numb58_2) == false);
-    Console.WriteLine((numb23 != numb21_18) == true);
+    //Тест в виде: Выводит на экран всегда true, если результат совпадает с ожидаемым
+    //Команда в формате: ВыводНаЭкран(func() == ожидаемый_результат);
 
-    //<
-    Console.WriteLine("\nТест команды <");
-    Console.WriteLine((numb58_1 < numb23) == true);
-    Console.WriteLine("======");
-    Console.WriteLine((numb58_1 == numb58_2) == true);
-    Console.WriteLine(numb58_1 == numb58_2);
-    Console.WriteLine((numb58_1 < numb58_2) == false);
-    Console.WriteLine("======");
-    Console.WriteLine((numb23 < numb21_18) == true);
+    try
+    {
 
-    //>
-    Console.WriteLine("\nТест команды >");
-    Console.WriteLine((numb58_1 > numb23) == false);
-    Console.WriteLine((numb58_1 > numb58_2) == false);
-    Console.WriteLine((numb23 > numb21_18) == false);
+        //==
+        Console.WriteLine("\nТест команды ==");
+        Console.WriteLine((numb58_1 == numb23) == false);
+        Console.WriteLine((numb58_1 == numb58_2) == true);
+        Console.WriteLine((numb23 == numb21_18) == false);
 
-    //<=
-    Console.WriteLine("\nТест команды <=");
-    Console.WriteLine((numb58_1 <= numb23) == true);
-    Console.WriteLine((numb58_1 <= numb58_2) == true);
-    Console.WriteLine((numb23 <= numb21_18) == true);
+        //!=
+        Console.WriteLine("\nТест команды !=");
+        Console.WriteLine((numb58_1 != numb23) == true);
+        Console.WriteLine((numb58_1 != numb58_2) == false);
+        Console.WriteLine((numb23 != numb21_18) == true);
 
-    //>=
-    Console.WriteLine("\nТест команды >=");
-    Console.WriteLine((numb58_1 >= numb23) == false);
-    Console.WriteLine((numb58_1 >= numb58_2) == true);
-    Console.WriteLine((numb23 >= numb21_18) == false);
+        //<
+        Console.WriteLine("\nТест команды <");
+        Console.WriteLine((numb58_1 < numb23) == true);
+        Console.WriteLine("======");
+        Console.WriteLine((numb58_1 == numb58_2) == true);
+        Console.WriteLine(numb58_1 == numb58_2);
+        Console.WriteLine((numb58_1 < numb58_2) == false);
+        Console.WriteLine("======");
+        Console.WriteLine((numb23 < numb21_18) == true);
 
-    //+
-    Console.WriteLine("\nТест команды +");
-    tmp.Numerator = 31;
-    tmp.Denominator = 24;
-    Console.WriteLine((numb58_1 + numb23) == tmp); // 31/24
+        //>
+        Console.WriteLine("\nТест команды >");
+        Console.WriteLine((numb58_1 > numb23) == false);
+        Console.WriteLine((numb58_1 > numb58_2) == false);
+        Console.WriteLine((numb23 > numb21_18) == false);
 
-    tmp.Numerator = 10;
-    tmp.Denominator = 8;
-    Console.WriteLine((numb58_1 + numb58_2) == tmp); // 10/8
+        //<=
+        Console.WriteLine("\nТест команды <=");
+        Console.WriteLine((numb58_1 <= numb23) == true);
+        Console.WriteLine((numb58_1 <= numb58_2) == true);
+        Console.WriteLine((numb23 <= numb21_18) == true);
 
-    tmp.Numerator = 33;
-    tmp.Denominator = 18;
-    Console.WriteLine((numb23 + numb21_18) == tmp); // 33/18
+        //>=
+        Console.WriteLine("\nТест команды >=");
+        Console.WriteLine((numb58_1 >= numb23) == false);
+        Console.WriteLine((numb58_1 >= numb58_2) == true);
+        Console.WriteLine((numb23 >= numb21_18) == false);
 
-    //-
-    Console.WriteLine("\nТест команды -");
-    tmp.Numerator = -1;
-    tmp.Denominator = 24;
-    Console.WriteLine((numb58_1 - numb23) == tmp); // -1/24
+        //+
+        Console.WriteLine("\nТест команды +");
+        tmp.Numerator = 31;
+        tmp.Denominator = 24;
+        Console.WriteLine((numb58_1 + numb23) == tmp); // 31/24
 
-    tmp.Numerator = 0;
-    tmp.Denominator = 8;
-    Console.WriteLine((numb58_1 - numb58_2) == tmp); // 0/8
+        tmp.Numerator = 10;
+        tmp.Denominator = 8;
+        Console.WriteLine((numb58_1 + numb58_2) == tmp); // 10/8
 
-    tmp.Numerator = -9;
-    tmp.Denominator = 18;
-    Console.WriteLine((numb23 - numb21_18) == tmp); // -9/18
+        tmp.Numerator = 33;
+        tmp.Denominator = 18;
+        Console.WriteLine((numb23 + numb21_18) == tmp); // 33/18
 
-    //++
-    Console.WriteLine("\nТест команды ++");
-    tmp.Numerator = numb23.Numerator + numb23.Denominator;
-    tmp.Denominator = numb23.Denominator;
-    numb23++;
-    Console.WriteLine(numb23 == tmp);
+        //-
+        Console.WriteLine("\nТест команды -");
+        tmp.Numerator = -1;
+        tmp.Denominator = 24;
+        Console.WriteLine((numb58_1 - numb23) == tmp); // -1/24
 
-    tmp.Numerator = numb58_1.Numerator + numb58_1.Denominator;
-    tmp.Denominator = numb58_1.Denominator;
-    numb58_1++;
-    Console.WriteLine(numb58_1 == tmp);
+        tmp.Numerator = 0;
+        tmp.Denominator = 8;
+        Console.WriteLine((numb58_1 - numb58_2) == tmp); // 0/8
 
-    tmp.Numerator = numb21_18.Numerator + numb21_18.Denominator;
-    tmp.Denominator = numb21_18.Denominator;
-    numb21_18++;
-    Console.WriteLine(numb21_18 == tmp);
+        tmp.Numerator = -9;
+        tmp.Denominator = 18;
+        Console.WriteLine((numb23 - numb21_18) == tmp); // -9/18
 
-    //--
-    Console.WriteLine("\nТест команды --");
-    tmp.Numerator = numb23.Numerator - numb23.Denominator;
-    tmp.Denominator = numb23.Denominator;
-    numb23--;
-    Console.WriteLine(numb23 == tmp);
+        //++
+        Console.WriteLine("\nТест команды ++");
+        tmp.Numerator = numb23.Numerator + numb23.Denominator;
+        tmp.Denominator = numb23.Denominator;
+        numb23++;
+        Console.WriteLine(numb23 == tmp);
 
-    tmp.Numerator = numb58_1.Numerator - numb58_1.Denominator;
-    tmp.Denominator = numb58_1.Denominator;
-    numb58_1--;
-    Console.WriteLine(numb58_1 == tmp);
+        tmp.Numerator = numb58_1.Numerator + numb58_1.Denominator;
+        tmp.Denominator = numb58_1.Denominator;
+        numb58_1++;
+        Console.WriteLine(numb58_1 == tmp);
 
-    tmp.Numerator = numb21_18.Numerator - numb21_18.Denominator;
-    tmp.Denominator = numb21_18.Denominator;
-    numb21_18--;
-    Console.WriteLine(numb21_18 == tmp);
+        tmp.Numerator = numb21_18.Numerator + numb21_18.Denominator;
+        tmp.Denominator = numb21_18.Denominator;
+        numb21_18++;
+        Console.WriteLine(numb21_18 == tmp);
 
-    //SetDefault(ref numb23, ref numb58_1, ref numb58_2, ref numb21_18);
+        //--
+        Console.WriteLine("\nТест команды --");
+        tmp.Numerator = numb23.Numerator - numb23.Denominator;
+        tmp.Denominator = numb23.Denominator;
+        numb23--;
+        Console.WriteLine(numb23 == tmp);
 
-    //ToString()
-    Console.WriteLine("\nТест команды ToString()");
-    Console.WriteLine("numb23 = " + numb23);
-    Console.WriteLine("numb58_1 = " + numb58_1);
-    Console.WriteLine("numb21_18 = " + numb21_18);
+        tmp.Numerator = numb58_1.Numerator - numb58_1.Denominator;
+        tmp.Denominator = numb58_1.Denominator;
+        numb58_1--;
+        Console.WriteLine(numb58_1 == tmp);
 
-    //float
-    Console.WriteLine("\nТест команды (float)");
-    Console.WriteLine("(float)numb23 = " + (float)numb23);
-    Console.WriteLine("(float)numb58_1 = " + (float)numb58_1);
-    Console.WriteLine("(float)numb21_18 = " + (float)numb21_18);
+        tmp.Numerator = numb21_18.Numerator - numb21_18.Denominator;
+        tmp.Denominator = numb21_18.Denominator;
+        numb21_18--;
+        Console.WriteLine(numb21_18 == tmp);
 
-    //int
-    Console.WriteLine("\nТест команды (int)");
-    Console.WriteLine("(int)numb23 = " + (int)numb23);
-    Console.WriteLine("(int)numb58_1 = " + (int)numb58_1);
-    Console.WriteLine("(int)numb21_18 = " + (int)numb21_18);
+        //SetDefault(ref numb23, ref numb58_1, ref numb58_2, ref numb21_18);
 
-    //*
-    Console.WriteLine("\nТест команды *");
-    Console.WriteLine((numb58_1 * numb23) == new RationalNumber(10, 24));
-    Console.WriteLine("======");
-    Console.WriteLine(numb58_1 * numb58_2);
-    Console.WriteLine((numb58_1 * numb58_2) == new RationalNumber(25, 64));
-    Console.WriteLine("======");
-    Console.WriteLine((numb23 * numb21_18) == new RationalNumber(42, 54));
+        //ToString()
+        Console.WriteLine("\nТест команды ToString()");
+        Console.WriteLine("numb23 = " + numb23);
+        Console.WriteLine("numb58_1 = " + numb58_1);
+        Console.WriteLine("numb21_18 = " + numb21_18);
 
-    //"/"
-    Console.WriteLine("\nТест команды /");
-    Console.WriteLine((numb58_1 / numb23) == new RationalNumber(15, 16));
-    Console.WriteLine((numb58_1 / numb58_2) == new RationalNumber(40, 40));
-    Console.WriteLine((numb23 / numb21_18) == new RationalNumber(36, 63));
+        //float
+        Console.WriteLine("\nТест команды (float)");
+        Console.WriteLine("(float)numb23 = " + (float)numb23);
+        Console.WriteLine("(float)numb58_1 = " + (float)numb58_1);
+        Console.WriteLine("(float)numb21_18 = " + (float)numb21_18);
 
-    //%
-    Console.WriteLine("\nТест команды %");
-    Console.WriteLine((numb58_1 % numb23) == new RationalNumber(15, 16));
-    Console.WriteLine((numb58_1 % numb58_2) == new RationalNumber(0, 40));
-    Console.WriteLine((numb23 % numb21_18) == new RationalNumber(36, 63));
+        //int
+        Console.WriteLine("\nТест команды (int)");
+        Console.WriteLine("(int)numb23 = " + (int)numb23);
+        Console.WriteLine("(int)numb58_1 = " + (int)numb58_1);
+        Console.WriteLine("(int)numb21_18 = " + (int)numb21_18);
+
+        //*
+        Console.WriteLine("\nТест команды *");
+        Console.WriteLine((numb58_1 * numb23) == new RationalNumber(10, 24));
+        Console.WriteLine("======");
+        Console.WriteLine(numb58_1 * numb58_2);
+        Console.WriteLine((numb58_1 * numb58_2) == new RationalNumber(25, 64));
+        Console.WriteLine("======");
+        Console.WriteLine((numb23 * numb21_18) == new RationalNumber(42, 54));
+
+        //"/"
+        Console.WriteLine("\nТест команды /");
+        Console.WriteLine((numb58_1 / numb23) == new RationalNumber(15, 16));
+        Console.WriteLine((numb58_1 / numb58_2) == new RationalNumber(40, 40));
+        Console.WriteLine((numb23 / numb21_18) == new RationalNumber(36, 63));
+
+        //%
+        Console.WriteLine("\nТест команды %");
+        Console.WriteLine((numb58_1 % numb23) == new RationalNumber(15, 16));
+        Console.WriteLine((numb58_1 % numb58_2) == new RationalNumber(0, 40));
+        Console.WriteLine((numb23 % numb21_18) == new RationalNumber(36, 63));
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+
 }
-catch (Exception e)
-{
-    Console.WriteLine(e.Message);
-}
+
+TestComplexNumber();
